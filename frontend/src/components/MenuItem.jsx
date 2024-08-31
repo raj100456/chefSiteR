429a6f36bf7c41cc9431f7b7cd7ccc50b5d8bb0a
raj100456 <patelrajkumar579@gmail.com>
2024-08-31 15:52:44 +0530

    menu component grid

--- frontend/src/components/MenuItem.jsx
+++ frontend/src/components/MenuItem.jsx
@@ -7,10 +7,10 @@ import Rectangle14 from "../assets/images/Rectangle14.png";
 const images = [Group5, image6, Rectangle13, Rectangle14];
 
 const MenuItem = () => (
-  <div className="flex flex-wrap ml-[150px]">
+  <div className="grid grid-cols-2 grid-rows-2 ml-[150px]">
     {images.map((src, index) => (
-      <div key={index}>
-        <img src={src} alt="menuitem" />
+      <div key={index} className="w-full">
+        <img src={src} alt="menuitem" className="w-full" />
       </div>
     ))}
   </div>
